b6ca46b27a3c15796bd612bf3d41d601b84ecbf1
Luana Stela <luana.arantes.lsa@gmail.com>
2022-11-21 15:30:19 -0300

    first commit

---
+++ anatomia-das-classes/MinhaClasse.java
@@ -0,0 +1,20 @@
+public class MinhaClasse {
+
+public static void main (String [] args) {
+    
+    String primeiroNome = "Luana";
+    String segundoNome = "Stela";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
+
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+
+}
+
+}
